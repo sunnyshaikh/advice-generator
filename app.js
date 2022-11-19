37096ca96ec9796d5831133de53cac9295e2c3f7
AltafAlam Shaikh <gitforaltaf@gmail.com>
2022-11-19 23:47:15 +0530

    updates

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ btn.addEventListener('click', getAdvice)
 
 // get advice function
 async function getAdvice() {
+  // fetching advices from below API
   const data = await fetch('https://api.adviceslip.com/advice');
   const res = await data.json();
 
+  // populate advice in html elements
   document.getElementById('advice-id').innerText = res.slip.id;
   document.getElementById('advice-text').innerText = res.slip.advice;
 }
